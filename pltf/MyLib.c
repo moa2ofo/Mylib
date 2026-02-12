4d8e9236084d2e996b1fceb9360b07d7726ace95
Andrea Monti <131896606+moa2ofo@users.noreply.github.com>
2026-02-12 11:32:40 +0100

    unit has been auto generated

--- pltf/MyLib.c
+++ pltf/MyLib.c
@@ -118,12 +118,12 @@ uint8_t MyLib_UpdateCounter_u8(uint32_t add_u32) {
 
   l_CycleCnt_u32++;
 
-  if(g_systemReady_b == false) {
+  if (g_systemReady_b == false) {
     l_ret_u8 = 1U;
   } else {
     l_new_u32 = g_counter_u32 + add_u32;
 
-    if((SaturationEn_b == true) && (l_new_u32 > CounterLimit_u32)) {
+    if ((SaturationEn_b == true) && (l_new_u32 > CounterLimit_u32)) {
       g_counter_u32 = CounterLimit_u32;
       l_ret_u8 = 2U;
     } else {
@@ -132,12 +132,11 @@ uint8_t MyLib_UpdateCounter_u8(uint32_t add_u32) {
     }
 
     /* Periodically toggle saturation enable to exercise both internal static variables. */
-    if((l_CycleCnt_u32 & 0x0FU) == 0U) {
+    if ((l_CycleCnt_u32 & 0x0FU) == 0U) {
       SaturationEn_b = (SaturationEn_b == true) ? false : true;
     }
   }
 
   return l_ret_u8;
 }
-
 int main() { return 0; }
