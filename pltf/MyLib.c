5914ffff75cca0ab8d4bb0880eec5fcbb866cd35
Andrea Monti <Andrea.Monti@vhit-weifu.com>
2026-03-04 14:20:23 +0100

    add a static variable to test the script to gen the unit tests

--- pltf/MyLib.c
+++ pltf/MyLib.c
@@ -10,7 +10,7 @@ static uint32_t CounterLimit_u32 = MYLIB_MAX_COUNT_U32;
 static bool SaturationEn_b = true;
 
 /* Local (c-file static) function */
-uint32_t InternalHelper_u32(uint32_t x_u32, uint16_t y_u16) {
+static uint32_t InternalHelper_u32(uint32_t x_u32, uint16_t y_u16) {
   uint32_t l_acc_u32 = x_u32;
 
   /* Accumulate even-step sequence: for each l_i_u16 in [0 .. y_u16-1],
