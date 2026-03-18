d120f1e9b95fb779e9449699e51fb60aef9f4ae4
Andrea Monti <Andrea.Monti@vhit-weifu.com>
2026-03-18 14:52:04 +0100

    update git actions

--- dev/docs/html/search/functions_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['updatecounter_5fu8_0',['updatecounter_u8',['../MyLib_8c.html#aa19af382ea69ad8ab8de7b6d582b1562',1,'UpdateCounter_u8(uint32_t add_u32):&#160;MyLib.c'],['../MyLib_8h.html#aa19af382ea69ad8ab8de7b6d582b1562',1,'UpdateCounter_u8(uint32_t add_u32):&#160;MyLib.c']]]
-];
