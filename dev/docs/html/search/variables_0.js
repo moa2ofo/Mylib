ab687304388b35bed8f51cbf7dcabe52bf3d7f02
Andrea Monti <Andrea.Monti@vhit-weifu.com>
2026-03-18 13:40:46 +0100

    delete docs added documentation creations scripts

--- dev/docs/html/search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['counterlimit_5fu32_0',['CounterLimit_u32',['../MyLib_8c.html#ad81e9442fb06ea224edc3055131c5ef6',1,'MyLib.c']]]
-];
--- dev/docs/html/search/variables_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['id_5fu16_0',['id_u16',['../structMyLib__record__t.html#a9a6dce19d8cf804b57197cc56c99cf09',1,'MyLib_record_t']]]
-];
