1e87423a97a2be2ac2b01a478407b2d9234a5c60
Andrea Monti <Andrea.Monti@vhit-weifu.com>
2026-03-18 16:04:47 +0100

    delete results

--- dev/docs/html/search/all_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['processrecord_0',['processrecord',['../MyLib_8c.html#a09e209253fe184eddc89f13fbf751b50',1,'ProcessRecord(const MyLib_record_t *rec_pc, uint8_t multiplier_u8):&#160;MyLib.c'],['../MyLib_8h.html#a09e209253fe184eddc89f13fbf751b50',1,'ProcessRecord(const MyLib_record_t *rec_pc, uint8_t multiplier_u8):&#160;MyLib.c']]]
-];
