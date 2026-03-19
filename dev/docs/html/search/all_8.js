759715880d63e098a1e94e7e8a22267d2be57402
Andrea Monti <Andrea.Monti@vhit-weifu.com>
2026-03-19 08:55:51 +0100

    delete results

--- dev/docs/html/search/all_8.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['value_5fu32_0',['value_u32',['../structMyLib__record__t.html#a45afba76c8e6d381eaa9014fdf488307',1,'MyLib_record_t']]]
-];
--- dev/docs/html/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['analyzearray_5fu32_0',['analyzearray_u32',['../MyLib_8c.html#a9a012e582e10fbe8211f3dec731d71f5',1,'AnalyzeArray_u32(uint16_t *values_pu16, size_t len_u32, uint16_t factor_u16):&#160;MyLib.c'],['../MyLib_8h.html#a9a012e582e10fbe8211f3dec731d71f5',1,'AnalyzeArray_u32(uint16_t *values_pu16, size_t len_u32, uint16_t factor_u16):&#160;MyLib.c']]]
-];
