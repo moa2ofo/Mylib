64079bf0157b6ecb7b1a3c5d024ae5c16720debb
Andrea Monti <Andrea.Monti@vhit-weifu.com>
2026-06-05 14:49:48 +0200

    delete unit tests

--- test/unitTest/TEST_MyLib_AnalyzeArray_u32/src/MyLib_AnalyzeArray_u32_help.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEST_MYLIB_ANALYZEARRAY_U32_HELP_H
-#define TEST_MYLIB_ANALYZEARRAY_U32_HELP_H
-
-#include "MyLib_AnalyzeArray_u32.h"
-#include <stddef.h>
-#include <string.h>
-
-#endif /* TEST_MYLIB_ANALYZEARRAY_U32_HELP_H */
