1d289e565736af647e9461e72b8e2da5cff45322
Alessandro Salvato <external.alessandro.salvato@vhit-weifu.com>
2026-06-05 15:20:23 +0200

    Delete all source files from unit test folder

--- test/unitTest/TEST_unit_bf679415_main/src/unit_bf679415_main.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEST_UNIT_BF679415_MAIN_H
-#define TEST_UNIT_BF679415_MAIN_H
-
-#include "MyLib.h"
-
-int main(void);
-
-#endif /* TEST_UNIT_BF679415_MAIN_H */
--- test/unitTest/TEST_unit_bf679415_main/src/unit_bf679415_main_help.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEST_UNIT_BF679415_MAIN_HELP_H
-#define TEST_UNIT_BF679415_MAIN_HELP_H
-
-#include "unit_bf679415_main.h"
-#include <stddef.h>
-#include <string.h>
-
-#endif /* TEST_UNIT_BF679415_MAIN_HELP_H */
