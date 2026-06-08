c412543d65c9620371eec960e39be67a050e8322
Alessandro Salvato <external.alessandro.salvato@vhit-weifu.com>
2026-06-08 10:18:59 +0200

    Delete all source files from unit test folder

--- test/unitTest/TEST_unit_bf679415_AnalyzeArray_u32/src/unit_bf679415_AnalyzeArray_u32_help.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEST_UNIT_BF679415_ANALYZEARRAY_U32_HELP_H
-#define TEST_UNIT_BF679415_ANALYZEARRAY_U32_HELP_H
-
-#include "unit_bf679415_AnalyzeArray_u32.h"
-#include <stddef.h>
-#include <string.h>
-
-#endif /* TEST_UNIT_BF679415_ANALYZEARRAY_U32_HELP_H */
--- test/unitTest/TEST_unit_bf679415_main/src/unit_bf679415_main.c
+++
@@ -1,6 +0,0 @@
-#include "unit_bf679415_main_help.h"
-
-/* FUNCTION TO TEST */
-int main() {
-  return 0;
-}
